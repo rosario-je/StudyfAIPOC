f6558103662d268814dcc1a6c3c2732269444b1e
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-11-11 20:27:03 -0500

    Fix JSON parsing comment in server setup

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ import db from './db/connection.js';
 const app = express();
 
 
-app.use(express.json()); parse JSON
+app.use(express.json());
 app.use(cors({
   origin: 'http://localhost:5173', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
